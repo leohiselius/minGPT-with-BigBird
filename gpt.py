898d10ad99109498df1e37bc17e97f2a7e1a9914
leohiselius <58816607+leohiselius@users.noreply.github.com>
2020-10-09 13:26:55 +0200

    Add files via upload

--- gpt.py
+++ gpt.py
@@ -12,18 +12,24 @@ class GPT(Module):
 
 class MultiHeadSelfAttention(Module):
 
-    def __init__(self, n_embd = 256, n_heads = 8, bigBird = False):
+    def __init__(self, n_embd = 784, n_heads = 12, n_k = 64, bigBird = False):
         super(SelfAttention, self).__init__()                       # ???
 
         self.n_embd = n_embd
+        self.n_heads = n_heads
+        self.n_k = n_k
 
-        self.W_k = Linear(n_embd, n_embd)
-        self.W_q = Linear(n_embd, n_embd)
-        self.W_v = Linear(n_embd, n_embd)
+        self.W_k = Linear(n_k*n_heads, n_embd)     #torch applies the transpose of Linear ( W_k(x) = x @ W_k.T ) 
+        self.W_q = Linear(n_k*n_heads, n_embd)
+        self.W_v = Linear(n_k*n_heads, n_embd)
+
+        self.W0 = Linear(n_k, n_k*n_heads)          #output projection (combines the 12 heads into 1)
+        self.register_buffer("mask", torch.tril(torch.ones(config.block_size, config.block_size))   #OBS SNODD KOD... vad ska block_size vara?
+                                     .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, X):
 
-        K = self.W_k(X)
+        K = self.W_k(X)                             # = X @ W_k.T
         Q = self.W_q(X)
         V = self.W_v(X)
 
